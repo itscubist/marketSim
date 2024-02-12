fe5992b7a33cfbf8280ac17d98620c697f2d2330
itscubist <baranbodur@yahoo.com.tr>
2024-02-12 14:22:33 -0500

    renamed marketSim to marketSimTest, since it is for now just testing functionality

--- marketSimTest.py
+++ marketSimTest.py
@@ -6,16 +6,26 @@ import supplySide as sup
 # Testing imported classes and their functionalities
 
 # 
-mat1 = sup.RawMaterial("wood")
-mat1.setRandomSupplyPars()
-mat1.increaseDemand(100)
-mat1.getPrice()
-mat1.printInfo()
+#mat1 = sup.RawMaterial("wood")
+#mat1.setRandomSupplyPars()
+#mat1.increaseDemand(100)
+#mat1.getPrice()
+#mat1.printInfo()
 
-ax = mat1.plotSupplyCurve()
-plt.show()
+#ax = mat1.plotSupplyCurve()
+#plt.show()
 
-mat1.printInfo()
+#mat1.printInfo()
 
 #supplyFormula = supplySide.genericSupplyFormula(10,2,12)
 #print(supplyFormula(100))
+
+# Create raw material objects with random supply pars
+matNameList = ["wood","clay","metal","wheat","meat"]
+matList = [sup.RawMaterial(s) for s in matNameList]
+for i in range(len(matList)):
+    matList.setRandomSupplyPars()
+    matList.printInfo()
+
+# Create product objects
+prodNameList = ["plank","brick","knife","flour","steak"]
